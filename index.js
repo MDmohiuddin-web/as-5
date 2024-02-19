129bb4e7cbbaaeac6936c2ea08c3896443dfec59
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-02-19 13:25:31 +0600

    add
    add

--- index.js
+++ index.js
@@ -69,19 +69,22 @@ couponBtn.addEventListener("click", () => {
 
 
 
+
 function buyTicket() {
-  const seatSection = document.getElementById("buyTicket");
+  const seatSection = document.getElementById("buyTickets");
   seatSection.scrollIntoView({ behavior: "smooth" });
   
 }
 
 function modalHide() {
   location.reload();
+  
 }
 
 
 function formSubmit(e) {
   e.preventDefault();
+ 
 
 
   let name = document.getElementById("name");
@@ -107,6 +110,44 @@ function formSubmit(e) {
 }
 
 
+// this is utility section
+
+function setBackground(elementId){
+  const element = document.getElementById(elementId)
+  element.classList.add('bg-[#1dd100]')
+}
+
+function getValue(elementId){
+  const element = document.getElementById(elementId);
+  const currentElementText = element.innerText;
+  const value = parseInt(currentElementText);
+
+  return value;
+}
 
 
+function setValue(elementId, value){
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
+
+function setTableData(seatNo){
+const table = document.getElementById("ticketTable");
+const newRow = document.createElement("tr");
+
+const cell1 = document.createElement("td");
+const cell2 = document.createElement("td");
+const cell3 = document.createElement("td");
+cell1.classList.add('py-2', 'tData')
+cell3.classList.add('text-right')
+cell1.textContent = seatNo;
+cell2.textContent = "Economy";
+cell3.textContent = "550";
+newRow.appendChild(cell1);
+newRow.appendChild(cell2);
+newRow.appendChild(cell3);
+table.appendChild(newRow);
+
+}
 
